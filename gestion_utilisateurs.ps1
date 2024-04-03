e917e00cb95d353d19ced25225041cbb920d6147
Zakaria Farhati <zakariafarhati18@gmail.com>
2024-04-03 11:15:56 +0000

    First commit

---
+++ gestion_utilisateurs.ps1
@@ -0,0 +1,100 @@
+﻿#Administration des comptes utilisateurs et groupes locaux en PowerShell
+
+#=============================les commandes du Modules Localaccounts========================
+
+Clear-Host
+#Get-Command -Module Microsoft.PowerShell.LocalAccounts
+
+#================================mise à jour de l'aide du module localaccounts===============
+
+#Update-Help -Module Microsoft.PowerShell.LocalAccounts
+
+#help Get-LocalUser
+#-Help Get-LocalUser -examples
+#-Help Get-LocalUser -Online
+
+
+#==================================Afficher les comptes utilisateurs locaux======================
+
+#Get-LocalUser
+#Get-LocalUser -Name admin
+#Get-LocalUser -Name admin | Select-Object *
+#Get-LocalUser -Name admin | Select-Object name,sid,objectclass |Format-Table
+#Get-LocalUser -Name admin | Select-Object name,sid,objectclass |Format-List
+
+
+
+#=====================================créer un nouveau compte utilisateur==================================
+
+#$password = Read-Host 'Donner le Mot de Passe ' -AsSecureString
+#New-LocalUser -Name utilisateur01 -Password $password -FullName "Stagiaire utilisateur01 " -Description "Stagiaire ID1A"
+
+
+#====================================Afficher tous les groupes locaux=============================================
+
+#Get-LocalGroup|Format-Table -AutoSize
+ 
+#===========================Ajouter utilisateur01 au groupe Utilisateurs=====utiliser "," pour ajouter plusieurs===========
+
+#Add-LocalGroupMember -Name utilisateurs -Member utilisateur01
+#Get-LocalGroupMember -Name utilisateurs
+
+#=======================créer un nouveau compte utilisateur local sans Mot de Passe (doit changer MPasse prochaine session)======
+
+#New-LocalUser -name utilisateur02 -NoPassword -FullName "Stagiaire Utilisateur02 " -Description "compte utilisateur ID1A"
+#Get-LocalUser -Name utilisateur02 | Select-Object *
+
+#============================================Ajouter utilisateur02 au groupe Utilisateurs=============
+
+#Add-LocalGroupMember -Name utilisateurs -Member utilisateur02 -Verbose
+#Get-LocalGroupMember -Name utilisateurs
+
+#===============================================Modifier les options de mot de passe===================
+
+#Get-LocalUser utilisateur01 | select *
+#Set-LocalUser -Name utilisateur01 -PasswordNeverExpires $true
+#Get-LocalUser utilisateur01 | select *
+
+#=================================Activer / Désactiver compte utilisateur====================================
+
+
+#Disable-LocalUser -Name utilisateur01
+#Get-LocalUser utilisateur01
+#Enable-LocalUser -Name utilisateur01
+#Get-LocalUser utilisateur01
+
+#==================================================Supprimer un utilisateur =======================
+
+#Remove-LocalUser -Name utilisateur02 
+
+#Get-LocalUser
+#Get-LocalGroupMember -Name utilisateurs |Format-Table
+
+#======================================Afficher les groupes locaux=========================================
+
+#Get-LocalGroup
+
+#============================créer un nouveau groupe local================================================
+
+#New-LocalGroup -Name G_ID1A -Description "Groupe local stagiaires ID1A"
+#Get-LocalGroup
+
+#====================Ajouter utilisateur01 en tant que membre du groupe G_ID1A==================================
+
+#Add-LocalGroupMember G_ID1A -Member utilisateur01
+
+#=============================Afficher les membres d'un groupe local============================================
+
+#Get-LocalGroupMember G_id1a| Select-Object name,objectclass
+
+#=============================Retirer un membre du groupe========================================================
+
+#Remove-LocalGroupMember -Group G_id1a -Member utilisateur01
+#Get-LocalGroupMember -Group G_id1a
+
+#=======================================Supprimer un groupe local======================================================
+
+#Remove-LocalGroup -Name G_ID1A 
+
+#Get-LocalGroup
+
---
+++ variables-operateurs-boucles_recherche_compte.ps1
@@ -0,0 +1,117 @@
+﻿Clear-Host
+#=============================Définition des Variables================================================
+
+$ve = 18
+$vch = 'x'
+$ve.GetType()
+$vch.GetType()
+$ve = $ve + 5
+echo $ve
+Write-Host $ve
+Write-Host $vch
+
+
+#====================================définition des constant==========================================
+New-Variable -Name PI -value 3.14 -Option Constant
+$pi = 4
+$pi.GetType()
+$vch.GetType()
+[int]$v1= Read-Host 'entrer un nombre entier'
+Write-Host $v1
+
+
+#=========================================================Opérateurs====================================
+#aréthmétique +,-,*,/,%
+#comparaison -eq -ne -gt -ge -lt -le
+#opérateur -in -not in
+#opérateur logiques -and -or -not ! -xor
+
+
+
+#======================================les Boucles==========================================================
+$nombre = 0
+$tab = 0..99
+$tab.Length
+while ($nombre -lt $tab.Length)
+{
+ $tab[$nombre]
+ $nombre++   
+}
+
+#======================================================la Boucle for=======================================
+$tab = 0..99
+for ($i = 0; $i -le 99; $i++)
+{ 
+ Write-Host $tab[$i]
+    
+}
+
+
+#=========================================la Boucle foreach===============================================
+
+#créer plusieurs comptes utilisateurs locaux et les ajouter au groupe utilisateurs=============================
+
+$nom_ut = "U_ID1A"
+$tab_ut = 1..4
+$password = Read-Host "Donner le Mot de Passe" -AsSecureString
+foreach ($i in $tab_ut)
+{
+New-LocalUser -Name $nom_ut$i -Password $password -Description "compte stagiaire $nom_ut$i "
+Add-LocalGroupMember utilisateurs -Member $nom_ut$i
+   
+}
+
+#==============================Compter le nombre d'utilisateurs=============================================
+
+$n_ut = Get-LocalUser 
+Write-Host "le Nombre d'utilisateurs = " -NoNewline
+$n_ut.Count
+(Get-LocalUser).count
+
+
+#===========================compter le nombre de comptes activés et désactivés=============================
+
+$vcomptes = Get-LocalUser  | Where-Object {$_.enabled -eq $true}
+$vcomptes = Get-LocalUser  | Where-Object enabled -EQ $true      #nouvelle notation
+Write-Host 'Nombre utilisateurs Activés = ' -NoNewline
+$vcomptes.Count
+$vcomptes|Format-Table -AutoSize
+$vcomptes = Get-LocalUser  | Where-Object {$_.enabled -eq $false}
+Write-Host 'Nombre utilisateurs Désactivés = ' -NoNewline
+$vcomptes.Count
+echo $vcomptes|Format-Table -AutoSize
+
+
+#===========================rechercher un compte par son nom s'il n'existe pas créer le==========================
+
+$nom_rech = Read-Host 'donner un nom de compte '
+$comptes = Get-LocalUser
+$trouve =$false
+foreach ($ut in $comptes)
+{
+    if ($ut.Name -eq $nom_rech)
+    {
+        Write-Warning " utilisateur existe déja "
+        $trouve = $true
+    }
+}
+if (!$trouve)
+{
+New-LocalUser -Name $nom_rech -NoPassword 
+}
+
+
+# ===================================une autre méthode==============================================
+
+$nom_rech = Read-Host 'donner un nom de compte '
+$vcomptes = Get-LocalUser  | Where-Object {$_.name -eq $nom_rech}
+if ($vcomptes.count -eq 0 )
+{
+  New-LocalUser -Name $nom_rech -NoPassword 
+    
+}
+else
+{
+ Write-Warning " utilisateur existe déja "   
+}
+
